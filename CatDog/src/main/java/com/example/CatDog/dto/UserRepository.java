b0b9cd2057e3bb8ecff456db732e5f55bad833b5
Alex Leventis <alex.leventis1@gmail.com>
2021-02-04 22:13:26 -0500

    get demo app working

---
+++ CatDog/src/main/java/com/example/CatDog/dto/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.CatDog.dto;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.CatDog.dto.User;
+
+
+
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+
+}
